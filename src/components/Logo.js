7191cddbe363f88bb65533078a74a61b987171cd
Nancy ZHeng <nancyzhe@bu.edu>
2022-04-19 20:32:55 -0400

    base of app, added sign in/sign up

---
+++ src/components/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Image, StyleSheet } from 'react-native'
+
+export default function Logo() {
+  return <Image source={require('../assets/logo.png')} style={styles.image} />
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 400,
+    height: 200,
+    marginBottom: -20,
+    marginTop: -125,
+  },
+})
+
---
+++ src/core/config.js
@@ -0,0 +1,11 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAN63jvjWEQTw7IKdPQNp4Th-yJN_tgUoU",
+  authDomain: "foodies-c6e6f.firebaseapp.com",
+  databaseURL: "https://foodies-c6e6f-default-rtdb.firebaseio.com",
+  projectId: "foodies-c6e6f",
+  storageBucket: "foodies-c6e6f.appspot.com",
+  messagingSenderId: "187086393672",
+  appId: "1:187086393672:web:9732d4512ca0c5baec637d",
+  measurementId: "G-GMTXKR697Q"
+}
