096d16a70074e9edee29709d78c203207373b3bb
Byron05 <byronmit@bu.edu>
2022-04-26 23:01:39 -0400

    Added the ability to access camera roll or camera

--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, Button, Image, View } from 'react-native';
 import SecondBackground from '../components/SecondBackground'
 import HeaderLogo from '../components/HeaderLogo'
 import Header from '../components/Header'
 import Paragraph from '../components/Paragraph'
-import Button from '../components/Button'
+// import Button from '../components/Button'
 import { logoutUser } from '../api/auth-api'
 import InfiniteScroll from 'react-infinite-scroll-component' 
 import FeedPic from '../components/FeedPic'
 
 export default function Feed() {
+
   return (
-  <SecondBackground>
+    <SecondBackground>
       <HeaderLogo />
       <Header>Foodies Feed</Header>
       <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
-        <Paragraph style={styles.userText}>
-        @byron
-      </Paragraph>
-      <FeedPic style={styles.pic}></FeedPic>
+          <Paragraph style={styles.userText}>
+            @byron
+          </Paragraph>
+          <FeedPic style={styles.pic}></FeedPic>
 
-      <Paragraph style={styles.userText}>
-        @jami
-      </Paragraph>
-      <FeedPic style={styles.pic}></FeedPic>
+          <Paragraph style={styles.userText}>
+            @jami
+          </Paragraph>
+          <FeedPic style={styles.pic}></FeedPic>
         </ScrollView>
       </SafeAreaView>
     </SecondBackground>
-    
-  ) }
-    
-    
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
--- src/screens/UploadScreen.js
+++ src/screens/UploadScreen.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';  
+import React, {useState} from 'react';
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, Button, Image, View } from 'react-native';
 import HeaderLogo from '../components/HeaderLogo';
 import SecondBackground from '../components/SecondBackground';
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 export default function UploadScreen() {
+
+    const [image, setImage] = useState(null)
+
+    const pickImage = async () => {
+  
+      const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY); //MEDIA_LIBRARY launchImageLibraryAsync  // CAMERA launchCameraAsync
+  
+      if (status != "granted") {
+        alert("We need permission to use your camera roll if you'd like to include a photo.");
+      }
+      else{
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3]
+        });
+  
+        console.log(result)
+  
+        if (!result.cancelled) {
+          setImage(result.uri )
+        }
+      }
+    }
+
     return (
-        <SecondBackground>
-            <HeaderLogo />
-        </SecondBackground>
-            
-     );
+        
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button title="Pick an image from camera roll" onPress={pickImage} />
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        </View>
+    );
 }
